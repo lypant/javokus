d0421b679bf041fba9cd1cfe21654d088cd38782
lypant <lypant@tlen.pl>
2015-12-27 12:01:34 +0100

    Add Transformation interface and BlockTransformation class
    Modify build.xml to use uninstrumented classes as well in class path - e.g. to find interfaces, which are not instrumented

---
+++ src/net/lypant/javokus/model/blocks/Transformation.java
@@ -0,0 +1,22 @@
+package net.lypant.javokus.model.blocks;
+
+public interface Transformation
+{
+    public enum State
+    {
+        NORMAL_0,
+        NORMAL_90,
+        NORMAL_180,
+        NORMAL_270,
+        MIRRORED_0,
+        MIRRORED_90,
+        MIRRORED_180,
+        MIRRORED_270
+    }
+    
+    public State getState();
+    public void setState(State state);
+    public void rotate();
+    public void mirror();
+}
+
