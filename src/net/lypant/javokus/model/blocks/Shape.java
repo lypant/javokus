ffccefec5f74b22812b0f117b89ef3d03a231ec2
lypant <lypant@tlen.pl>
2015-12-29 13:13:44 +0100

    Continue with Shape class

--- src/net/lypant/javokus/model/blocks/Shape.java
+++ src/net/lypant/javokus/model/blocks/Shape.java
@@ -5,13 +5,20 @@ import java.util.*;
 import static net.lypant.javokus.model.blocks.Transformable.Capability.*;
 import net.lypant.javokus.model.blocks.Transformable.*;
 
-public class Shape implements Transformable
+public class Shape implements Transformable, Iterable<Tile>
 {
+    private final static int MAX_TILE_COUNT = 5;
     private EnumSet<Capability> capabilities;
+    private Collection<Tile> tiles;
+    private Collection<Tile> readOnlyTilesView;
+    private Size size;
 
     public Shape(EnumSet<Capability> capabilities)
     {
         this.capabilities = capabilities;
+        tiles = new TreeSet<Tile>();
+        readOnlyTilesView = Collections.unmodifiableCollection(tiles);
+        size = new Size();
     }
 
     public Shape()
@@ -27,6 +34,34 @@ public class Shape implements Transformable
     public boolean isMirrorable()
     {
         return capabilities.contains(MIRRORABLE);
+    }
+
+    public Iterator<Tile> iterator()
+    {
+        return readOnlyTilesView.iterator();
+    }
+
+    public void addTile(Tile tile)
+    {
+        assert tiles.size() < MAX_TILE_COUNT : "max tile count already reached: " + MAX_TILE_COUNT;
+
+        tiles.add(tile);
+        size.extendWithTile(tile);
+    }
+
+    public int getTileCount()
+    {
+        return tiles.size();
+    }
+
+    public int getWidth()
+    {
+        return size.getWidth();
+    }
+
+    public int getHeight()
+    {
+        return size.getHeight();
     }
 }
 
--- tst/net/lypant/javokus/model/blocks/ShapeTest.java
+++ tst/net/lypant/javokus/model/blocks/ShapeTest.java
@@ -86,4 +86,162 @@ public class ShapeTest
         Transformable transformable = new Shape(EnumSet.of(ROTATEABLE, MIRRORABLE));
         assertTrue(transformable.isMirrorable());
     }
+
+    @Test
+    public void noTilesCheckCount()
+    {
+        Shape shape = new Shape();
+        assertEquals(0, shape.getTileCount());
+    }
+
+    @Test
+    public void oneTileCheckCount()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        assertEquals(1, shape.getTileCount());
+    }
+
+    @Test
+    public void twoTilesCheckCount()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        assertEquals(2, shape.getTileCount());
+    }
+
+    @Test
+    public void threeTilesCheckCount()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        assertEquals(3, shape.getTileCount());
+    }
+
+    @Test
+    public void fourTilesCheckCount()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        shape.addTile(new Tile(0, 3));
+        assertEquals(4, shape.getTileCount());
+    }
+
+    @Test
+    public void fiveTilesCheckCount()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        shape.addTile(new Tile(0, 3));
+        shape.addTile(new Tile(0, 4));
+        assertEquals(5, shape.getTileCount());
+    }
+
+    @Test(expected = AssertionError.class)
+    public void tooManyTiles()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        shape.addTile(new Tile(0, 3));
+        shape.addTile(new Tile(0, 4));
+        shape.addTile(new Tile(1, 0));
+    }
+
+    @Test
+    public void getIterator()
+    {
+        Shape shape = new Shape();
+        assertNotNull(shape.iterator());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void tryToRemoveTile()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        Iterator<Tile> it = shape.iterator();
+        it.next();
+        it.remove();
+    }
+
+    @Test
+    public void iterate()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        shape.addTile(new Tile(0, 3));
+        shape.addTile(new Tile(0, 4));
+
+        int counter = 0;
+        for(Tile tile : shape)
+        {
+            ++counter;
+        }
+        assertEquals(5, counter);
+    }
+
+    @Test
+    public void widthInitial()
+    {
+        Shape shape = new Shape();
+        assertEquals(0, shape.getWidth());
+    }
+
+    @Test
+    public void widthOneTile()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        assertEquals(1, shape.getWidth());
+    }
+
+    @Test
+    public void widthFiveTiles()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(1, 0));
+        shape.addTile(new Tile(2, 0));
+        shape.addTile(new Tile(3, 0));
+        shape.addTile(new Tile(4, 0));
+        assertEquals(5, shape.getWidth());
+    }
+
+    @Test
+    public void heightInitial()
+    {
+        Shape shape = new Shape();
+        assertEquals(0, shape.getHeight());
+    }
+
+    @Test
+    public void heightOneTile()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        assertEquals(1, shape.getHeight());
+    }
+
+    @Test
+    public void heightFiveTiles()
+    {
+        Shape shape = new Shape();
+        shape.addTile(new Tile(0, 0));
+        shape.addTile(new Tile(0, 1));
+        shape.addTile(new Tile(0, 2));
+        shape.addTile(new Tile(0, 3));
+        shape.addTile(new Tile(0, 4));
+        assertEquals(5, shape.getHeight());
+    }
 }
